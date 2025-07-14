c00ddd80beb0f5f97bdd08407629ffcd2f8c595f
eb-mars <84507581+eb-mars@users.noreply.github.com>
2025-07-14 14:46:28 +0100

    params loading
    added function to load params, and added example use of this function to 'run_script.py'

---
+++ load.py
@@ -0,0 +1,44 @@
+import os
+import glob
+from landlab import load_params
+
+
+def load_params(ext=".txt"):
+    """
+    Finds a .txt file in the current working directory 
+    and loads it using the load_params function from landlab.
+
+    Args:
+        ext (str): extension for the param file desired (string), e.g. 'txt' (default)
+
+    Returns:
+        params (dict): Parameters loaded from the .txt file.
+    
+    Raises:
+        FileNotFoundError: If no .txt files are found.
+        TypeError: If the loaded parameters are not a dictionary.
+    """
+    if not isinstance(ext, str):
+        raise TypeError("The 'ext' argument must be a string (e.g., 'txt').");
+    
+    ext = ext if ext.startswith(".") else f".{ext}";
+
+    cwd = os.getcwd(); ## current working directory (probably maybe be the run# ????)
+    paramfile = glob.glob(cwd+"/*{}".format(ext)); ## find the params file (of whatever extension, e.g. txt) in the current folder
+    
+    ## No file found?? --> Raise an error 
+    if not paramfile:
+        raise FileNotFoundError("No {} parameter file found in the current directory.".format(ext));
+
+    params = load_params(paramfile); ## load the params file (using landlab load_params)
+    
+    ## File not loaded as desired format --> Raise an error
+    if not isinstance(params, dict):
+        raise TypeError("Expected 'params' to be a dictionary.");
+
+    return params
+
+
+
+
+
